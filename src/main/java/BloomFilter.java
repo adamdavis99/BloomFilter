95c9247342bfd454203f42f01650c69659911160
adamdavis99 <aruneshsarker1970@gmail.com>
2023-06-03 20:41:46 +0530

    Testing bloom filters with different number of hash functions

--- src/main/java/BloomFilter.java
+++ src/main/java/BloomFilter.java
@@ -1,24 +1,43 @@
 import org.apache.commons.codec.digest.MurmurHash3;
 
 import java.nio.charset.StandardCharsets;
+import java.util.Random;
 
 public class BloomFilter {
-    int size;
     byte[] filter;
-    BloomFilter(int size) {
-        filter = new byte[size / Byte.SIZE];
+    int size;
+    int[] seeds;
+    int numberOfSeeds;
+    BloomFilter(int size, int numberOfSeeds) {
         this.size = size;
+        filter = new byte[size / Byte.SIZE];
+        seeds = new int[numberOfSeeds];
+        this.numberOfSeeds = numberOfSeeds;
+        Random random = new Random();
+        for(int i = 0; i < numberOfSeeds; i++)
+            seeds[i] = random.nextInt();
     }
 
-    public void add(String key) {
-        int hash = Math.abs(MurmurHash3.hash32x86(key.getBytes(StandardCharsets.UTF_8))) % size;
-        filter[hash / 8] |= (byte)(1 << (hash % 8));
+    private int hashKey(String key, int idx) {
+        byte[] data = key.getBytes(StandardCharsets.UTF_8);
+        return Math.abs(MurmurHash3.hash32x86(data, 0, data.length, seeds[idx % numberOfSeeds])) % size;
     }
 
-    public boolean exists(String key) {
-        int hash = Math.abs(MurmurHash3.hash32x86(key.getBytes(StandardCharsets.UTF_8))) % size;
-       // System.out.println(key + " " + hash);
-        return ((filter[hash / 8] & (byte)(1 << (hash % 8))) != 0);
+    public void add(String key, int numberOfHashes) {
+        for(int i = 0; i < numberOfHashes; i++) {
+            int hash = hashKey(key, i);
+            filter[hash / 8] |= (byte)(1 << (hash % 8));
+        }
+    }
+
+    public boolean exists(String key, int numberOfHashes) {
+        for (int i = 0; i < numberOfHashes; i++) {
+            int hash = hashKey(key, i);
+            if (((filter[hash / 8] & (byte)(1 << (hash % 8))) == 0)) {
+                return false;
+            }
+        }
+        return true;
     }
 
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,50 +1,50 @@
+import org.apache.commons.codec.digest.MurmurHash3;
+
 import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-        BloomFilter filter = new BloomFilter(1000);
+        BloomFilter filter = new BloomFilter(1000, 1);
         String[] keys = {"A", "B", "C", "D", "E", "F", "G"};
         for(String key : keys) {
-            filter.add(key);
+            filter.add(key, 1);
         }
 
         for(String key : keys) {
-            System.out.println(filter.exists(key));
+            System.out.println(filter.exists(key, 1));
         }
-        System.out.println(filter.exists("Food"));
-        System.out.println(filter.exists("key"));
-        System.out.println(filter.exists("J")); // false positive
+        System.out.println(filter.exists("Food", 1));
+        System.out.println(filter.exists("key", 1));
+        System.out.println(filter.exists("J", 1)); // false positive
 
 
         Map<String, Boolean> data_exists = new HashMap<>();
         Map<String, Boolean> data_does_not_exists = new HashMap<>();
         List<String> dataset = new ArrayList<>();
 
-        for(int j = 1000; j <= 20000; j += 32) {
-            dataset.clear();
-            data_exists.clear();
-            data_does_not_exists.clear();
+        for(int i = 0; i < 500; i++) {
+            UUID uuid = UUID.randomUUID();
+            dataset.add(uuid.toString());
+            data_exists.put(uuid.toString(), true);
+        }
 
-            for(int i = 0; i < 500; i++) {
-                UUID uuid = UUID.randomUUID();
-                dataset.add(uuid.toString());
-                data_exists.put(uuid.toString(), true);
-            }
+        for(int i = 0; i < 500; i++) {
+            UUID uuid = UUID.randomUUID();
+            dataset.add(uuid.toString());
+            data_does_not_exists.put(uuid.toString(), true);
+        }
 
-            for(int i = 0; i < 500; i++) {
-                UUID uuid = UUID.randomUUID();
-                dataset.add(uuid.toString());
-                data_does_not_exists.put(uuid.toString(), true);
-            }
 
-            BloomFilter bigFilter = new BloomFilter(j);
+        for(int j = 1; j <= 100; j++) {
+
+            BloomFilter bigFilter = new BloomFilter(10000, 100);
             for(Map.Entry<String, Boolean> entry : data_exists.entrySet()) {
-                bigFilter.add(entry.getKey());
+                bigFilter.add(entry.getKey(), j);
             }
 
             int falsePositives = 0;
             for(String key : dataset) {
-                boolean exists = bigFilter.exists(key);
+                boolean exists = bigFilter.exists(key, j);
                 if(exists && data_does_not_exists.containsKey(key)) {
                     falsePositives ++;
                 }
